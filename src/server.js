9184013d504347bf1d7c90162dd8c1d55a5f3cc7
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-12-30 11:50:23 -0300

    Updating Cookie's destination

--- src/server.js
+++ src/server.js
@@ -16,7 +16,12 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://foodexplorer-development.netlify.app',
+      'https://foodexplorerfront.netlify.app'
+    ],
     credentials: true
   })
 )
